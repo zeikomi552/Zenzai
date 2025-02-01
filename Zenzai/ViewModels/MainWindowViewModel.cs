e4116e099fa7cc6d5d2c6644120ddfbeea5182fc
zeikomi552 <premium.tsubu@gmail.com>
2025-02-02 07:28:05 +0900

    画面終了時にWebUIを終了するよう修正

--- Zenzai/ViewModels/MainWindowViewModel.cs
+++ Zenzai/ViewModels/MainWindowViewModel.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zenzai.Models.A1111;
 
 namespace Zenzai.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        IWebUIControllerModel _WebuiCtrl;
+        public MainWindowViewModel(IWebUIControllerModel webUi)
+        {
+            _WebuiCtrl = webUi;
+        }
         public void Closing()
         {
-
+            _WebuiCtrl.CloseWebUI();
         }
     }
 
