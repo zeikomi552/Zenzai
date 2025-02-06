c60d63caf01890ab76fd699f7440e0b828add860
zeikomi552 <premium.tsubu@gmail.com>
2025-02-07 06:38:23 +0900

    マテリアルデザインの適用

--- Zenzai/Views/MainWindow.xaml.cs
+++ Zenzai/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Zenzai.Views
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
