df383e2f9afafba9ea49b68673d31e851b7505b6
zeikomi552 <premium.tsubu@gmail.com>
2025-02-07 17:15:25 +0900

    Zipにして保存する処理を作成

--- Zenzai/Common/Utilities/PathManager.cs
+++ Zenzai/Common/Utilities/PathManager.cs
@@ -86,5 +86,46 @@ namespace Zenzai.Common.Utilities
             return text;
         }
         #endregion
+
+        /// <summary>
+        /// ディレクトリをコピーする。
+        /// </summary>
+        /// <param name="sourceDirName">コピーするディレクトリ</param>
+        /// <param name="destDirName">コピー先のディレクトリ</param>
+        public static void CopyDirectory(string sourceDirName, string destDirName)
+        {
+            // コピー先のディレクトリがないかどうか判定する
+            if (!Directory.Exists(destDirName))
+            {
+                // コピー先のディレクトリを作成する
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // コピー元のディレクトリの属性をコピー先のディレクトリに反映する
+            File.SetAttributes(destDirName, File.GetAttributes(sourceDirName));
+
+            // ディレクトリパスの末尾が「\」でないかどうかを判定する
+            if (!destDirName.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                // コピー先のディレクトリ名の末尾に「\」を付加する
+                destDirName = destDirName + Path.DirectorySeparatorChar;
+            }
+
+            // コピー元のディレクトリ内のファイルを取得する
+            string[] files = Directory.GetFiles(sourceDirName);
+            foreach (string file in files)
+            {
+                // コピー元のディレクトリにあるファイルをコピー先のディレクトリにコピーする
+                File.Copy(file, destDirName + Path.GetFileName(file), true);
+            }
+
+            // コピー元のディレクトリのサブディレクトリを取得する
+            string[] dirs = Directory.GetDirectories(sourceDirName);
+            foreach (string dir in dirs)
+            {
+                // コピー元のディレクトリのサブディレクトリで自メソッド（CopyDirectory）を再帰的に呼び出す
+                CopyDirectory(dir, destDirName + Path.GetFileName(dir));
+            }
+        }
     }
 }
