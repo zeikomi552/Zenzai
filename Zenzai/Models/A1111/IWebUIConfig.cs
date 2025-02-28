1d9da39e94986ccc6fbf452910cb94540acd7858
zeikomi552 <premium.tsubu@gmail.com>
2025-03-01 05:51:03 +0900

    パラメータ設定を増やした

--- Zenzai/Models/A1111/IWebUIConfig.cs
+++ Zenzai/Models/A1111/IWebUIConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stdapi.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,21 @@ namespace Zenzai.Models.A1111
 
         public string Prompt { get; }
         public string NegativePrompt { get; }
+
+        public int Steps { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public decimal CfgScale { get; }
+
+        public string SamplerIndex {  get; }
+
+        public SamplerIndexEnum? Sampler { get; }
+
+        public int N_iter { get; }
+
+        public int BatchSize { get; }
+
+        public Int64 Seed { get; }
 
         public void SetParameters(IWebUIControllerModel ctrl);
     }
